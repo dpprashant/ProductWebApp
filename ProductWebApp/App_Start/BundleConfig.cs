903eb74277eabcb45a9b9d2631b5a40caaeb8de9
winds-sales001 <61082202+winds-sales001@users.noreply.github.com>
2021-03-27 21:28:49 +0530

    Add project files.

---
+++ ProductWebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ProductWebApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(                        
+                        "~/Content/js/move-top.js",
+                        "~/Content/js/easing.js",
+                        "~/Content/js/easyResponsiveTabs.js",
+                        "~/Content/js/tikslus360.min.js",
+                        "~/Content/js/rainbow.min.js",
+                        "~/Content/unitegallery/js/*.js",
+                        "~/Content/unitegallery/themes/default/ug-theme-default.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/style.css",
+                "~/Content/css/tikslus360.css",
+                "~/Content/unitegallery/css/unite-gallery.css",
+                "~/Content/unitegallery/themes/default/ug-theme-default.css"));
+        }
+    }
+}
---
+++ ProductWebApp/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult SendEmail(string receiver, string subject, string message)
+        public ActionResult SendEmail()
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    //MailMessage message = new MailMessage();
+                    //message.From = new MailAddress("dpprashant1@gmail.com");
+
+                    //message.To.Add(new MailAddress("info@risingtechlabs.com"));
+
+                    //message.Subject = "your subject";
+                    //message.Body = "content of your email";
+
+                    //SmtpClient client = new SmtpClient();
+                    //client.Send(message);
+
+                    var smptClient = new SmtpClient("smtp.gmail.com", 587)
+                    {
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential("dpprashant1@gmail.com", "9429715419@Heli"),
+                        EnableSsl = true                       
+                    };
+                    smptClient.Send("bjrisingtechlabs@gmail.com", "dpprashant1@gmail.com", "Testing Email", "testing the email");
+
+
+                    //var senderEmail = new MailAddress("bjrisingtechlabs@gmail.com", "PD");
+                    //var receiverEmail = new MailAddress("dpprashant1@gmail.com", "Receiver");
+                    //var password = "Jayesh@123";
+                    //var sub = "Test";
+                    //var body = "Testing";
+
+                    //var smtp = new SmtpClient
+                    //{
+                    //    Host = "smtp.gmail.com",
+                    //    Port = 587,
+                    //    EnableSsl = true,
+                    //    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    //    UseDefaultCredentials = false,
+                    //    Credentials = new NetworkCredential(senderEmail.Address, password)
+                    //};
+
+                    //using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    //{
+                    //    Subject = "Test email",
+                    //    Body = body
+                    //})
+                    //{
+                    //    smtp.Send(mess);
+                    //}
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+    }
+}
